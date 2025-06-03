e1af5bbcc4a20b94c931d37ee4ef7d4bfd6f19f3
Nicholas Hadler <nhadler@berkeley.edu>
2025-06-02 17:05:16 -0700

    Add docstring to __init__.py for module description and set an explicit re-export.

--- src/landscaper/__init__.py
+++ src/landscaper/__init__.py
@@ -1,2 +1,4 @@
-from .hessian import PyHessian
-from .landscape import LossLandscape
+"""Landscaper is a comprehensive Python framework designed for exploring the loss landscapes of deep learning models."""
+
+from .hessian import PyHessian as PyHessian
+from .landscape import LossLandscape as LossLandscape
