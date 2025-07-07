0cf15ee471ce5f49ad128cbc8369041f1c9d860d
Nicholas Hadler <nhadler@berkeley.edu>
2025-07-07 15:41:59 -0700

    Add copyright and licensing information  to src files

--- src/landscaper/compute.py
+++ src/landscaper/compute.py
@@ -1,5 +1,18 @@
 """Module for computing loss landscapes for PyTorch models."""
 
+# Landscaper Copyright (c) 2025, The Regents of the University of California, 
+# through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the 
+# U.S. Dept. of Energy), University of California, Berkeley, and Arizona State University. All rights reserved.
+
+# If you have questions about your rights to use or distribute this software, 
+# please contact Berkeley Lab's Intellectual Property Office at IPO@lbl.gov.
+
+# NOTICE. This Software was developed under funding from the U.S. Department of Energy and 
+# the U.S. Government consequently retains certain rights. As such, the U.S. Government has been
+# granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide 
+# license in the Software to reproduce, distribute copies to the public, prepare derivative works, 
+# and perform publicly and display publicly, and to permit others to do so.
+
 import copy
 from collections.abc import Callable
 from itertools import product
