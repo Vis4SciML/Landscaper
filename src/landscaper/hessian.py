31c27dad132f80548639bd93def70e7100956e44
Nicholas Hadler <nhadler@berkeley.edu>
2025-07-10 19:06:14 -0700

    Update hessian.py

--- src/landscaper/hessian.py
+++ src/landscaper/hessian.py
@@ -1,23 +1,20 @@
-"""PyHessian Hessian module."""
+"""PyHessian Hessian module.
 
-# @file Different utility functions
-# Copyright (c) Zhewei Yao, Amir Gholami
-# All rights reserved.
-# This file is part of PyHessian library.
-#
-# PyHessian is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# PyHessian is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with PyHessian.  If not, see <http://www.gnu.org/licenses/>.
-# *
+This code was copied and modified from PyHessian (https://github.com/amirgholami/PyHessian/blob/master/pyhessian/hessian.py).
+"""
+
+# Landscaper Copyright (c) 2025, The Regents of the University of California, 
+# through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the 
+# U.S. Dept. of Energy), University of California, Berkeley, and Arizona State University. All rights reserved.
+
+# If you have questions about your rights to use or distribute this software, 
+# please contact Berkeley Lab's Intellectual Property Office at IPO@lbl.gov.
+
+# NOTICE. This Software was developed under funding from the U.S. Department of Energy and 
+# the U.S. Government consequently retains certain rights. As such, the U.S. Government has been
+# granted for itself and others acting on its behalf a paid-up, nonexclusive, irrevocable, worldwide 
+# license in the Software to reproduce, distribute copies to the public, prepare derivative works, 
+# and perform publicly and display publicly, and to permit others to do so.
 
 from collections.abc import Callable, Generator
 from typing import Any
